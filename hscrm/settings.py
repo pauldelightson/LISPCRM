7eed02f5ac747e38ca9e2468965fdcb8cb47e7a9
delightson <paul@lisptechnologies.com>
2022-03-22 13:34:42 +0530

    commit

--- hscrm/settings.py
+++ hscrm/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'Whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
