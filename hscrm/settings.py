207fec9210e407a38d8129845974c0f6862b5075
delightson <paul@lisptechnologies.com>
2022-03-22 13:05:06 +0530

    commit

--- hscrm/settings.py
+++ hscrm/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ['lispcrm-2-68q95.ondigitalocean.app']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'Whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -88,8 +90,12 @@ WSGI_APPLICATION = 'hscrm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),    
     }
 }
 
@@ -135,6 +141,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"  
 ]
 STATIC_ROOT = "static_root"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 AUTH_USER_MODEL = 'leads.User'
--- runserver.sh
+++ runserver.sh
@@ -1,3 +1,4 @@
+python manage.py collectstatic --noinput
 python manage.py migrate
 
 gunicorn --worker-tmp-dir /dev/shm hscrm.wsgi
