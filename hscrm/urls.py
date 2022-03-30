21c11e34c5cbadaa14b50d263d1b7422f37dd0e4
delightson <paul@lisptechnologies.com>
2022-03-30 10:48:26 +0530

    commit

--- hscrm/urls.py
+++ hscrm/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
     path('password-reset-done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('passwrod-reset-complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
